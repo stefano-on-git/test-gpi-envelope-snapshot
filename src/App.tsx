1a6689ee188c28be6e1539fe855e1e09dab3ec9a
stefano-on-git <18739638+stefano-on-git@users.noreply.github.com>
2022-03-31 09:46:56 +0200

    moved b64ToBlob func to utilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
 import { useEffect, useState } from "react";
 // @ts-expect-error
 import ImageResize from "image-resize";
-import { formatBytes } from "./Utilities";
+import { formatBytes, b64toBlob } from "./Utilities";
 import "./styles.css";
 
 export default function App() {
   const [imgFile, setImgFile] = useState<File>();
   const [imgFileResized, setImgFileresized] = useState<Blob>();
   const [imgDataUrlResized, setImgDataUrlResized] = useState<string>();
-
-  const b64toBlob = (b64Data: string, contentType = "", sliceSize = 512) => {
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-
-    const blob = new Blob(byteArrays, { type: contentType });
-    return blob;
-  };
 
   useEffect(() => {
     if (imgFile) {
--- src/Utilities.ts
+++ src/Utilities.ts
@@ -1,11 +1,31 @@
 export function formatBytes(bytes: number, decimals = 2) {
-    if (bytes === 0) return '0 Bytes';
+  if (bytes === 0) return "0 Bytes";
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+export function b64toBlob(b64Data: string, contentType = "", sliceSize = 512) {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
 }
